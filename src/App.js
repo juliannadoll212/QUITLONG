786cc35a92d865990b91c248e304152a24ff156c
juliannadoll212 <laurenmars212@gmail.com>
2024-11-09 17:43:10 +0800

    initial upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Project from "./components/Project";
+import Skills from "./components/Skills";
+
+
+export default function App() {
+  return (
+   <main> 
+  <Navbar /> 
+  <About />
+  <Project />
+  <Contact />
+  <Skills />
+ 
+
+
+
+
+
+   </main>
+  );
+}
